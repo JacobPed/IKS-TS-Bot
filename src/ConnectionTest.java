fd5c293354e9cea6ac2d44dafc693b587568c28d
Q11x <q11x@q11x.net>
2015-04-21 00:13:54 +0200

    began work on ability to auto update application

--- src/ConnectionTest.java
+++ src/ConnectionTest.java
@@ -22,124 +22,123 @@ import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent;
  *
  */
 public class ConnectionTest {
-	
+
 	TsConnection tsConnection;
 	private TS3Api api;
-
-	/**
-	 * 
-	 */
-	public ConnectionTest() {
-		tsConnection = new TsConnection();
-		api = tsConnection.getApi();
-		
-		clientInfoExample();
-	}
-	
-	public void clientInfoExample() {
-		
-		api.sendChannelMessage("Greetings. You're beloved " + tsConnection.getNickname() + " is online!");
-
-		/*for (final Client c : api.getClients()) {
-			System.out.println(c.getNickname() + " in channel: "
-					+ api.getChannelInfo(c.getChannelId()).getName());
-		}*/
-		
-		api.registerAllEvents();
-		api.addTS3Listeners(new TS3Listener() {
-
-			public void onTextMessage(TextMessageEvent e) {
-				if (e.getTargetMode() == TextMessageTargetMode.CHANNEL) {// Only
-																			// react
-																			// to
-																			// channel
-																			// messages
-					if(e.getMessage().equals("ray")) {
-						api.sendPrivateMessage(api.getClientByNameExact("Ray Cooper", true).getId(), "Jeg er en fluffy bot, og jeg har ingen idé om hvordan jeg svare dig hvis du skriver til mig D:");
-					}
-					if (e.getMessage().equals("ping")) {
-						api.sendChannelMessage("pong");
-					}
-					if (e.getMessage().toLowerCase().contains("hello")) {
-						api.sendChannelMessage("Greetings " + e.getInvokerName());
-					}
-					if(e.getMessage().toLowerCase().contains("!exit")) {
-						api.sendChannelMessage("Goodby.");
-						System.exit(0);
-					}
-				}
-			}
-
-			public void onClientJoin(ClientJoinEvent e) {
-				//api.sendChannelMessage("Hey fancy man! I'm a crazy bot that does crazy shit!");
-				clientMoveMessages(e.getClientTargetId(), e.getClientId());
-			}
-
-			public void onServerEdit(ServerEditedEvent e) {
-				System.out.println("Server edited by " + e.getInvokerName());
-			}
-
-			public void onClientMoved(ClientMovedEvent e) {
-				System.out.println("Client has been moved " + e.getClientId());
-				System.out.println("Channel ID: " + e.getClientTargetId());
-				
-				clientMoveMessages(e.getClientTargetId(), e.getClientId());
-				//System.out.println(api.getClientInfo(e.getClientId()).getNickname());
-				//System.out.println(api.getClientInfo(e.getClientId()).getUniqueIdentifier());
-			}
-
-			public void onClientLeave(ClientLeaveEvent e) {
-				// ...
-
-			}
-
-			public void onChannelEdit(ChannelEditedEvent e) {
-				// ...
-
-			}
-
-			public void onChannelDescriptionChanged(
-					ChannelDescriptionEditedEvent e) {
-				// ...
-
-			}
-
-			public void onChannelCreate(ChannelCreateEvent e) {
-				
-			}
-
-			public void onChannelDeleted(ChannelDeletedEvent e) {
-				
-			}
-
-			public void onChannelMoved(ChannelMovedEvent e) {
-				
-			}
-
-			public void onChannelPasswordChanged(ChannelPasswordChangedEvent e) {
-				
-			}
-		});
-	}
-	
-	private void clientMoveMessages(int targetId, int clientId) {
-		if(targetId == 26) {
-			String nickname = api.getClientInfo(clientId).getNickname().toLowerCase();
-			if(nickname.contains("ray")) {
-				api.sendChannelMessage("Greetings my master, and welcome home.");
-			}
-			else if(nickname.contains("rena") || nickname.contains("alleice")) {
-				api.sendChannelMessage("Greetings o' american visitor from the magical islands");
-			}
-		}
-	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		new ConnectionTest();
+		new ConnectionTest(args);
 	}
-	
+
+	/**
+	 * 
+	 */private ConnectionTest(String[] input) {
+		 tsConnection = new TsConnection();
+		 api = tsConnection.getApi();
+		 
+		 api.sendChannelMessage("Greetings. You're beloved " + tsConnection.getNickname() + " is online!");
+		 for(String s : input) {
+			 api.sendChannelMessage(s);
+	        }
+		 clientInfoExample();
+	 }
+
+	 public void clientInfoExample() {
+
+		 /*for (final Client c : api.getClients()) {
+			System.out.println(c.getNickname() + " in channel: "
+					+ api.getChannelInfo(c.getChannelId()).getName());
+		}*/
+
+		 api.registerAllEvents();
+		 api.addTS3Listeners(new TS3Listener() {
+
+			 public void onTextMessage(TextMessageEvent e) {
+				 if (e.getTargetMode() == TextMessageTargetMode.CHANNEL) {// Only
+					 // react
+					 // to
+					 // channel
+					 // messages
+					 if(e.getMessage().equals("ray")) {
+						 api.sendPrivateMessage(api.getClientByNameExact("Ray Cooper", true).getId(), "Jeg er en fluffy bot, og jeg har ingen idé om hvordan jeg svare dig hvis du skriver til mig D:");
+					 }
+					 if (e.getMessage().equals("ping")) {
+						 api.sendChannelMessage("pong");
+					 }
+					 if (e.getMessage().toLowerCase().contains("hello")) {
+						 api.sendChannelMessage("Greetings " + e.getInvokerName());
+					 }
+					 if(e.getMessage().toLowerCase().contains("!exit")) {
+						 api.sendChannelMessage("Goodby.");
+						 System.exit(0);
+					 }
+				 }
+			 }
+
+			 public void onClientJoin(ClientJoinEvent e) {
+				 //api.sendChannelMessage("Hey fancy man! I'm a crazy bot that does crazy shit!");
+				 clientMoveMessages(e.getClientTargetId(), e.getClientId());
+			 }
+
+			 public void onServerEdit(ServerEditedEvent e) {
+				 System.out.println("Server edited by " + e.getInvokerName());
+			 }
+
+			 public void onClientMoved(ClientMovedEvent e) {
+				 System.out.println("Client has been moved " + e.getClientId());
+				 System.out.println("Channel ID: " + e.getClientTargetId());
+
+				 clientMoveMessages(e.getClientTargetId(), e.getClientId());
+				 //System.out.println(api.getClientInfo(e.getClientId()).getNickname());
+				 //System.out.println(api.getClientInfo(e.getClientId()).getUniqueIdentifier());
+			 }
+
+			 public void onClientLeave(ClientLeaveEvent e) {
+				 // ...
+
+			 }
+
+			 public void onChannelEdit(ChannelEditedEvent e) {
+				 // ...
+
+			 }
+
+			 public void onChannelDescriptionChanged(
+					 ChannelDescriptionEditedEvent e) {
+				 // ...
+
+			 }
+
+			 public void onChannelCreate(ChannelCreateEvent e) {
+
+			 }
+
+			 public void onChannelDeleted(ChannelDeletedEvent e) {
+
+			 }
+
+			 public void onChannelMoved(ChannelMovedEvent e) {
+
+			 }
+
+			 public void onChannelPasswordChanged(ChannelPasswordChangedEvent e) {
+
+			 }
+		 });
+	 }
+
+	 private void clientMoveMessages(int targetId, int clientId) {
+		 if(targetId == 26) {
+			 String nickname = api.getClientInfo(clientId).getNickname().toLowerCase();
+			 if(nickname.contains("ray")) {
+				 api.sendChannelMessage("Greetings my master, and welcome home.");
+			 }
+			 else if(nickname.contains("rena") || nickname.contains("alleice")) {
+				 api.sendChannelMessage("Greetings o' american visitor from the magical islands");
+			 }
+		 }
+	 }	
 }
---
+++ src/TsUpdater.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+
+/**
+ * @author Jacob
+ *
+ */
+public class TsUpdater {
+
+	/*	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//new ConnectionTest();
+	}*/
+
+	/**
+	 * 
+	 */
+	public TsUpdater() {
+		// TODO Auto-generated constructor stub
+	}
+	public static void main(String[] args) {
+		String[] arr = { "Server updated!" };
+		//ConnectionTest.main(arr);
+	}
+
+}
