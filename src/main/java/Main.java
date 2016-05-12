7e968c355f80126499a572ea28e12241b02524a7
Jacob Pedersen <q11x@q11x.net>
2016-05-12 18:03:42 +0200

    initial main

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+/**
+ * Created by Jacob Pedersen (Q11x)) on 22-04-2016.
+ */
+public class Main {
+    public static void main(String[] args) {
+        //How args work: java ProgramName FirstArgument SecondArgument
+        //String one = args[0]; //=="FirstArgument"
+        //String two = args[1]; //=="SecondArgument"
+        //new Main(args);
+    }
+}
