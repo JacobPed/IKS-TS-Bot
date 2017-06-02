d8de13acd60984da7c10cee97ffb534d1e978049
Jacob Pedersen <jacob@jacobpedersen.dk>
2017-06-02 23:28:46 +0200

    Add test code for working with slaves - It's not possible
    From all attempt performed, it seems like it's only posible to have use a query login one at a time. Even if you allow it in group persmissions, and add ip to whitelist. It will always get banned because of rate limit.

--- src/main/kotlin/Master.kt
+++ src/main/kotlin/Master.kt
@@ -1,12 +1,16 @@
 import com.github.theholywaffle.teamspeak3.api.event.TS3EventAdapter
 import com.github.theholywaffle.teamspeak3.api.event.TextMessageEvent
+import com.sun.org.apache.xalan.internal.xsltc.util.IntegerArray
+import java.lang.Thread.yield
 import java.util.*
+import kotlin.concurrent.thread
 
 /**
  * Created by jacob on 2017-05-27 (YYYY-MM-DD).
  */
 class Master: TeamSpeakBot {
-    private var slaves = mutableListOf<ITeamSpeakBot>()
+//    private var slaves = mutableListOf<ITeamSpeakBot>()
+    private var slaves = mutableListOf<Thread>()
     private val eventQueue = LinkedList<Object>()
 
     constructor(config: Config): super(config){
@@ -16,15 +20,24 @@ class Master: TeamSpeakBot {
            setupSlaves()
     }
 
-    private fun setupSlaves() {
+    private fun setupSlaves(): Unit {
         val channels = api.channels
-        for (c in channels) {
+//        for (c in channels) {
+////        for (c in intArrayOf(1,2)) {
             // TODO: Create a new bot slave for each channel
-            val number = if(slaves.size > 0) slaves.size-1 else 0
-            val config = Config(this.config.LoginName, this.config.LoginPassword, this.config.NickName + "slave$number", ChannelId = c.id)
-            val newSlave = Slave(config, eventQueue)
+//            val number = if(slaves.size > 0) slaves.size-1 else 0
+            val number = 0
+//            val config = Config(this.config.LoginName, this.config.LoginPassword, this.config.NickName + "slave$number", ChannelId = 0)
+        try {
+
+
+            val config = Config("eoijwetoiohiewoih", "7lVlLwK4", this.config.NickName + "slave$number", ChannelId = 0)
+            val newSlave = thread() { Slave(config, eventQueue) }
             slaves.add(newSlave)
         }
+        catch (e: Exception){ println("slave creation exception: \n" + e)}
+            println("slave: $number")
+//        }
     }
 
     private fun setupEventListeners() {
