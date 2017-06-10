bcd7f53566fd94f2f7f1f1c21f8673711a1a2a13
Jacob Pedersen <jacob@jacobpedersen.dk>
2017-06-10 20:51:38 +0200

    Added loading of modules
    I really really tried to make it work with reflection. But I clearly don't understand that well enough for now.

--- src/main/kotlin/app/Main.kt
+++ src/main/kotlin/app/Main.kt
@@ -3,10 +3,18 @@ package app
 import modules.*
 import com.natpryce.konfig.*
 import java.io.File
+//import kotlin.reflect.KClass
+//import kotlin.reflect.full.cast
+//import kotlin.reflect.full.createInstance
+//import kotlin.reflect.full.createType
+//import kotlin.reflect.full.primaryConstructor
 
 /**
  * Created by jacob on 2017-05-27 (YYYY-MM-DD).
  */
+// The type inside should be AbstractModule, but it wont accept it for now. Star works as a workaround.
+//private val modules: List<KClass<*>> = listOf(StandardCommands::class)
+private var modules: MutableList<AbstractModule> = mutableListOf()
 
 fun main(args: Array<String>) {
     println("Hello, world!")
@@ -15,7 +23,7 @@ fun main(args: Array<String>) {
     val eventHandler = EventHandler(tsConnection)
     tsConnection.setupEventListeners(eventHandler)
     eventHandler.addModule(StandardCommands(tsConnection))
-    loadModules(eventHandler)
+    loadModules(eventHandler, tsConnection)
 //    bot.addModule(SimpleMath(bot))
     while (tsConnection.isConnected()) {
         Thread.yield()
@@ -25,7 +33,19 @@ fun main(args: Array<String>) {
     tsConnection.close()
 }
 
-fun loadModules(eventHandler: EventHandler) {
+fun loadModules(eventHandler: EventHandler, tsConnection: ITsConnection) {
+    //This is a shitty solution. But it'll do for now.
+    modules.add(StandardCommands(tsConnection))
+    modules.forEach { eventHandler.addModule(it) }
+
+//    modules.first().cast(AbstractModule::class)
+//    modules.first().constructors.apply { tsConnection }
+//    modules.first().createType()
+
+//    println(modules.first().createInstance())
+//    val module: AbstractModule = modules.first()::members as AbstractModule
+//    if(module != null)
+//        println("module: " + module.CommandPrefixes.first())
 }
 
 fun GetConfig(): Config {
